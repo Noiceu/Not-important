6bfb55f87d05cc58f64c204d4bf760229c3b775e
Noiceu <32655134+Noiceu@users.noreply.github.com>
2017-10-17 15:06:46 +1100

    The work
    :art:

---
+++ Group project/js/game.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+  /var game = new Phaser.Game(800, 600, Phaser.AUTO, '',{ preload: preload, create: create, update: update});
+var player
+var player2
+var controls
+  function preload(){
+  //remeber to add name of sprite here
+game.load.spritesheet('player', 'assets/')
+game.load.spritesheet('player2', 'assets/')
+  }
+  function create() {
+
+  }
+  function update() {
+
+  }
+};
